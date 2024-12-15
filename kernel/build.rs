e4f735aaaccf40b9f8273da23054221d52613198
Caznix <caznix01@gmail.com>
2024-12-15 00:32:26 -0500

    add rust-toolchain.toml

--- kernel/build.rs
+++ kernel/build.rs
@@ -8,6 +8,7 @@ fn main() {
         .spawn();
 
     let arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    println!("cargo:warn=test");
     println!("cargo:rustc-link-arg=-Tkernel/lds/{arch}-qemu.ld");
     println!("cargo:rerun-if-changed={arch}-qemu.ld");
 }
